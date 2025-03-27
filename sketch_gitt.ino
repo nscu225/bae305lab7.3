742a722c4ba6d7e0366c63c53d0863321c672ad7
nscu225 <nscu225@uky.edu>
2025-03-26 23:16:25 -0400

    Revert "Update sketch"
    This reverts commit 5f9f94488caf19b14749143563217924de33e56b.

--- sketch_gitt.ino
+++ sketch_gitt.ino
@@ -1,8 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
-const int PWMB = 10; //speed control pin on the motor driver for the left motor
-const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
-const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
+
 }
 
 void loop() {
